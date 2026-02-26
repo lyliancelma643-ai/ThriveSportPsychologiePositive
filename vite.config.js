44f4f7d2ab7898f33960ba9b2bd1f7c8dbc35f43
lylian <lyliancelma@MacBookAir-de-lylian.local>
2026-02-25 19:19:32 -0500

    chore: add vite-plugin-sitemap setup

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: './',
-    plugins: [react()],
+    plugins: [
+        react(),
+        Sitemap({
+            hostname: 'https://thrivetutorpositive.com',
+            dynamicRoutes: [
+                '/methode',
+                '/programmes',
+                '/sport',
+                '/evaluation',
+                '/booking',
+                '/zones',
+                '/a-propos'
+            ],
+            changefreq: 'monthly',
+            priority: 0.8,
+            generateRobotsTxt: false // On a déjà créé notre robots.txt sur mesure
+        })
+    ],
     server: {
         host: true, // Exposes the server to the network (crucial for iPad access)
     },
