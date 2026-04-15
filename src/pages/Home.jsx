93b954285a331fd07ae4574a5cc4e4525ebb6d52
lylian <lyliancelma@MacBookAir-de-lylian.local>
2026-04-15 15:11:15 -0400

    Update H1 slogan and remove testimonials from Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,6 @@ import AuthorityBar from '../components/home/AuthorityBar';
 import PainPoints from '../components/home/PainPoints';
 import Comparison from '../components/home/Comparison';
 import MethodTeaser from '../components/home/MethodTeaser';
-import Testimonials from '../components/home/Testimonials';
 import Faq from '../components/home/Faq';
 import Cta from '../components/home/Cta';
 import ContactSection from '../components/ui/ContactSection';
@@ -80,7 +79,6 @@ const Home = ({ setBookingStep, handleSelectProgram }) => {
             <MethodTeaser />
             <Logistics />
             <Locations />
-            <Testimonials />
             <ParentCommitment />
             <Faq />
             <div id="contact-section">
